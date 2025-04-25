1fbdd03831d0bd76f8b259c2cf1fec5ab9197c5c
Iramm Hamdard <Hamdard@fzi.de>
2025-04-25 23:44:06 +0200

    Add save_yaml_wo_overwriting function and update .gitignore
    Introduce a new utility function to prevent overwriting existing YAML files by merging data with previous content if the file exists. Additionally, update the .gitignore to include exceptions for the `logreplay` folder.

--- opencood/hypes_yaml/yaml_utils.py
+++ opencood/hypes_yaml/yaml_utils.py
@@ -371,3 +371,22 @@ def load_lift_splat_shoot_params(param):
     param['postprocess'].update({'anchor_args': anchor_args})
 
     return param
+
+
+def save_yaml_wo_overwriting(data, save_name):
+    """
+    Save the yaml file without overwriting the existing one.
+
+    Parameters
+    ----------
+    data : dict
+        The dictionary contains all data.
+
+    save_name : string
+        Full path of the output yaml file.
+    """
+    if os.path.exists(save_name):
+        prev_data = load_yaml(save_name)
+        data = {**data, **prev_data}
+
+    save_yaml(data, save_name)
